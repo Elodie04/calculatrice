8bd822270cfffed25ebcfe1d58eb6c16b395cebf
Elodie04 <Elodielefevre2@hotmail.com>
2024-12-27 12:43:16 +0100

    premier commit

---
+++ calculatrice.py
@@ -0,0 +1,9 @@
+#Début du programme 
+
+premierNombre = int(input("Veuillez entrer un premier nombre ="))
+secondNombre = int(input("Veuillez entrer un deuxième nombre = "))
+
+resultat = premierNombre + secondNombre
+
+#Fin du programme
+print(f"Le résultat de l'addition est : {premierNombre} + {secondNombre} = {resultat}")
